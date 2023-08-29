9b8ce5ec6dc7ff70e29213e59115dd9758edec84
Min <wyes91622@gmail.com>
2023-08-30 01:50:13 +0800

    chore: file naming uses upper camel case

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import UrlForm from "@/components/UrlForm";
-import GradientLayout from "@/layouts/gradientLayout";
+import GradientLayout from "@/layouts/GradientLayout";
 
 export default function Index() {
   return (
