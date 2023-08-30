4d2d4e9966dce4c6501d6605ae88d3a4376ebfef
Min <wyes91622@gmail.com>
2023-08-31 01:58:42 +0800

    feat: combine and adjust all style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import UrlForm from "@/components/UrlForm";
+import Banner from "@/components/Banner";
 import GradientLayout from "@/layouts/GradientLayout";
 
 export default function Index() {
   return (
-    <GradientLayout>
+    <GradientLayout className="py-60 px-32">
+      <Banner />
       <UrlForm />
     </GradientLayout>
   );
