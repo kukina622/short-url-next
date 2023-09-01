2ab3eb53b0b3e463b261a2ceca9590d26d91c0ee
Min <wyes91622@gmail.com>
2023-09-02 01:34:07 +0800

    feat: add short link dialog

---
+++ src/components/ShortLinkDialog.tsx
@@ -0,0 +1,86 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+
+interface IShortLinkDialogProps {
+  isOpen: boolean;
+  isSuccess?: boolean;
+  link?: string;
+  onClose: () => void;
+}
+
+const ShortLinkDialog = ({
+  isOpen,
+  isSuccess,
+  link,
+  onClose,
+}: IShortLinkDialogProps) => {
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-xl font-medium leading-6 text-gray-900"
+                >
+                  {isSuccess ? "Success" : "Failure"}
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-lg text-gray-500">{link}</p>
+                </div>
+
+                <div className="mt-8">
+                  <button
+                    type="button"
+                    className="mr-2 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    onClick={onClose}
+                  >
+                    Close
+                  </button>
+                  {isSuccess && (
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-purple-100 px-4 py-2 text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-purple-500 focus-visible:ring-offset-2"
+                      onClick={onClose}
+                    >
+                      Copy Link
+                    </button>
+                  )}
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+ShortLinkDialog.defaultProps = {
+  isSuccess: false,
+  link: "Operation failed",
+};
+
+export default ShortLinkDialog;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import UrlForm from "@/components/UrlForm";
 import Banner from "@/components/Banner";
 import GradientLayout from "@/layouts/GradientLayout";
+import ShortLinkDialog from "@/components/ShortLinkDialog";
 
 export default function Index() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <GradientLayout className="py-60 px-32">
       <Banner />
       <UrlForm />
+      <ShortLinkDialog isOpen={isOpen} onClose={() => setIsOpen(false)} />
     </GradientLayout>
   );
 }
