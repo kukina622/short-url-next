996f43790b82f430278f5836586a14e1a8cfbdd9
Min <wyes91622@gmail.com>
2023-09-03 21:15:06 +0800

    feat: add post link to generate shortUrl

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,3 +3,10 @@ import crypto from "crypto";
 export function generateRandomString(length = 6): string {
   return crypto.randomBytes(32).toString("base64url").slice(0, length);
 }
+
+export function isValidUrl(url: string): boolean {
+  const regex =
+    /^(https?):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]/;
+
+  return regex.test(url);
+}
